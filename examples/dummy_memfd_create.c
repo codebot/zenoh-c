ef25e5e1465268af35c6e416e52d69cd9daf9910
Morgan Quigley <morgan@osrfoundation.org>
2023-03-09 14:05:40 -0500

    add parameter names to prevent compiler warning

--- examples/dummy_memfd_create.c
+++ examples/dummy_memfd_create.c
@@ -3,7 +3,7 @@ This is just a placeholder to allow the Rust nix library
 to have something to link against on older systems.
 */
 
-int memfd_create(const char *, unsigned int)
+int memfd_create(const char *unused_name, unsigned int unused_flags)
 {
   return -1;
 }
